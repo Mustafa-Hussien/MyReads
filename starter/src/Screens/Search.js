9e26a214e19a7ff416592c1059491d3cba353f6c
Mustafa Hussien <mustafa.hussien100@gmail.com>
2023-01-01 22:37:12 +0200

    Adding UseEffect on Search screen

--- starter/src/Screens/Search.js
+++ starter/src/Screens/Search.js
@@ -1,9 +1,37 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import search from "../BooksAPI";
+import * as BookAPI from "../BooksAPI";
 import Book from "../Components/Book";
 
 const Search = ({ updateBookShelf, books }) => {
+  const [query, setQuery] = useState("");
+  const [searchedBooks, setSearchedBooks] = useState(books);
+  console.log(searchedBooks);
+
+  const updateQuery = (newValue) => {
+    setQuery(newValue);
+  };
+
+  useEffect(() => {
+    const searchBook = async () => {
+      const res = await BookAPI.search(query);
+      console.log(res);
+      setSearchedBooks(res);
+    };
+
+    searchBook();
+  }, [query]);
+
+  // const updateQuery = (d) => {
+  //   setQuery(d);
+  //   const setSearch = async () => {
+  //     const res = await BookAPI.search(query);
+  //     setSearchedBooks(res);
+  //     console.log(searchedBooks);
+  //   };
+  //   setSearch();
+  // };
+
   return (
     <div className="search-books">
       <div className="search-books-bar">
@@ -11,7 +39,12 @@ const Search = ({ updateBookShelf, books }) => {
           Close
         </Link>
         <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title, author, or ISBN" />
+          <input
+            type="text"
+            placeholder="Search by title, author, or ISBN"
+            value={query}
+            onChange={(e) => updateQuery(e.target.value)}
+          />
         </div>
       </div>
       <div className="search-books-results">
